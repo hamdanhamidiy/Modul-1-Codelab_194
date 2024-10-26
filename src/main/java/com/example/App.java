5a22498b03cd5908dff4efad4085d3391a11a975
hamdanhamidiy <hamdanhamidiy2687@gmail.com>
2024-10-27 02:23:01 +0700

    Add Dependency

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class App{
+    public static void main(String[]args){
+        String text="Hello,Maven!";
+        String reversedText=StringUtils.reverse(text);
+        System.out.println(reversedText);
+    }
+}
